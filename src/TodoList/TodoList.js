5343215274e1ed614c46c8654cd7494082151afb
KingBard77 <badamin771@gmail.com>
2022-08-03 09:45:46 +0800

    init

---
+++ src/TodoList/TodoList.js
@@ -0,0 +1,81 @@
+import './todolist.css'
+import React from 'react'
+
+class TodoList extends React.Component{
+  constructor (){
+    super();
+    this.state = {task:[],};
+
+    this.addTask = this.addTask.bind(this);
+    this.createTask = this.createTask.bind(this);
+    this.removeTask = this.removeTask.bind(this);
+    }
+
+    addTask(a){
+      if (this.textinput.value !== ''){
+        var newTask = {
+          add: this.textinput.value,
+          key:Date.now()
+      };
+    }
+
+    this.setState((prevState) => {
+      return {
+        task: prevState.task.concat(newTask)
+      };
+    });
+    this.textinput.value = '';
+    a.preventDefault();
+  }
+
+  createTask(b){
+    return(
+      <div className="deletelist">
+        <li key={b.key}>{b.add}
+          <button type='submit' onClick={() => this.removeTask(b.key)}>
+            Delete</button>
+        </li>
+      </div>
+    )
+  }
+
+  removeTask(c){
+    var filteredItems = this.state.task.filter(function (d){
+      return (d.key !== c)
+    });
+
+    this.setState({task:filteredItems});
+  }
+
+  render(){
+    var list = this.state.task;
+    var listtask = list.map(this.createTask);
+
+    return(
+      <div className='header'>
+        <div className='taskbox'>
+          <h1>Todo Application</h1>
+          <form>
+            <label>Task: </label>
+            <input name='added_task' ref={(a) => this.textinput = a}
+            placeholder="Enter your task"/>
+
+            <button type='submit' onClick={this.addTask}>Add</button>
+          </form>
+        </div>
+
+        <div className="list">
+          <p>List of Task</p>
+          
+          <hr/> 
+          
+          <div className='list_item'>
+            <ul>{listtask}</ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TodoList;
